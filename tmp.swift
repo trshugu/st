aa692108296990ae893c73a3a8d9f3267eaf2988
anotkuta <anotkuta@samnm.yalk>
2015-12-11 18:12:44 +0900

    gitpush2015/12/11

--- tmp.swift
+++ tmp.swift
@@ -1,7 +1,17 @@
 
+
+var vava = 1
+let lele = 2
+
+
+var vava = 3
+let lele = 4
+
+
+
 // コメント
 // println("hell swift") もうない
-print("hell swift")
+//print("hell swift")
 // println("hell swift")
 
 /*
