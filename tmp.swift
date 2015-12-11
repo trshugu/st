eb888fc3ab0b0332a6dfb5407b72bfca9234a330
anotkuta <anotkuta@samnm.yalk>
2015-12-11 18:15:51 +0900

    gitpush2015/12/11

--- tmp.swift
+++ tmp.swift
@@ -1,5 +1,45 @@
+var hoge = 55 // Int型
+var fuga = 55.0 // Double型
+var piyo = "hoge" // String型
+var poke = "h" //Character型ですよー
+var mon = true //真偽型ですよー
+
+//Array(配列)型ですよー
+var array = [1, 2, 3, 4, 5]
+
+//Dictionary(辞書)型ですよー
+var dict = [
+  "apple": "りんご",
+  "orange": "みかん"
+]  
+// ※括弧が{}ではなく[]なのに注意
+var hoge:Int = 55 //Int型ですよー
+var fuga:Double = 55 //Double型ですよー
+var piyo:String = "hoge" //String型ですよー
+var poke:Character = "h" //Character型ですよー
+var mon:Bool = true //真偽型ですよー
+
+print(hoge) //実行結果は"55"になる
+print(figa) //実行結果は"55.0"になる
+print(piyo) //実行結果は"hoge"になる
+print(poke) //実行結果は"h"になる
+print(mon) //実行結果は"true"になる
 
 
+var array : [Int] = [1, 2, 3, 4, 5] //Array(配列)型ですよー
+var emptyArray : [Int] = [] //空のArray(配列)型ですよー
+
+//Dictionary(辞書)型ですよー
+var dict : [String:String] = [
+  "apple": "りんご",
+  "orange": "みかん"
+]
+var emptyDict : [String:String] = [:] //空のDictionary(辞書)型ですよー
+
+
+
+
+/*
 var vava = 1
 print(vava)
 let lele = 2
@@ -11,7 +51,7 @@ print(vava)
 
 // lele = 4
 
-
+*/
 
 // コメント
 // println("hell swift") もうない
